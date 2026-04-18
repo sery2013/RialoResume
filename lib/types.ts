4ba11d7e8c9a84568ef21416c73fd459dd1951c1
SERY2013.ETH <101403669+sery2013@users.noreply.github.com>
2026-04-18 16:36:33 +0300

    Create types.ts

---
+++ lib/types.ts
@@ -0,0 +1,12 @@
+export interface ResumeProfile {
+  name: string
+  github: string
+  twitter: string
+  repos: number
+  commits: number
+  followers: number
+  lastSync: string
+  privacy: boolean
+  reactive: boolean
+  syncing: boolean
+}
